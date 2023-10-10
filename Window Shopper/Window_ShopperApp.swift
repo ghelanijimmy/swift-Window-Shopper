72b184c20dfa8721b47d9715416c284c1a905f5b
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-10 07:03:01 -0400

    Initial Commit

---
+++ Window Shopper/Window_ShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Window_ShopperApp.swift
+//  Window Shopper
+//
+//  Created by Jimmy Ghelani on 2023-10-10.
+//
+
+import SwiftUI
+
+@main
+struct Window_ShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
